c9445990a3d3fd301765a50b89850f79e0dbd5ef
Andy Sanchez <andysanchez726@gmail.com>
2020-06-09 23:45:54 -0500

    added most of the inquirer prompts so they can be ready for routes

---
+++ index.js
@@ -0,0 +1,100 @@
+const inquirer = require('inquirer');
+const options = ["View All Departments", "View All Employees", "View All Roles", "Add A Department", "Add A Role", "Add An Employee", "Update An Employee Role"]
+function Prompt() {
+
+    return inquirer
+        .prompt(
+            {
+                type: 'list',
+                name: 'selection',
+                message: 'What would you like to do?',
+                choices: options
+            }
+        )
+        .then(({selection}) => {
+            if (selection === options[0]) {
+                console.log('View All Departments was selected')
+                Prompt();
+            }
+            else if (selection === options[1]) {
+                console.log('View All Employees was selected')
+                Prompt();
+            }
+            else if (selection === options[2]) {
+                console.log('View All Roles was selected')
+                Prompt();
+            }
+            else if (selection === options[3]) {
+                console.log('Add A Department was selected')
+                return inquirer
+                    .prompt({
+                        type: 'text',
+                        name: 'department',
+                        message: 'What is the name of the department?'
+                    }).then(({department}) => {
+                        console.log(department)
+                        Prompt();
+                    })
+            }
+            else if (selection === options[4]) {
+                console.log('Add A Role was selected')
+                return inquirer
+                    .prompt([{
+                        type: 'text',
+                        name: 'role',
+                        message: 'What is the role?'
+                    },
+                    {
+                        type: 'text',
+                        name: 'salary',
+                        message: 'What is the salary for this role?'
+                    },
+                    {
+                        type: 'text',
+                        name: 'department',
+                        message: 'What department is this role apart of?'
+                    }
+                    ]).then(({role, salary, department}) => {
+                        console.log(role, salary, department)
+                        Prompt();
+                    })
+            }
+            else if (selection === options[5]) {
+                console.log('Add An Employee was selected')
+                return inquirer
+                    .prompt([{
+                        type: 'text',
+                        name: 'firstName',
+                        message: "What is the employee's first name?"
+                    },
+                    {
+                        type: 'text',
+                        name: 'lastName',
+                        message: "What is the employee's last name?"
+                    },
+                    {
+                        type: 'text',
+                        name: 'role',
+                        message: "What is the employee's role?"
+                    },
+                    {
+                        text: 'text',
+                        name: 'manager',
+                        message: "Who is the employee's manager?"
+                    }
+                    ]).then(({firstName, lastName, role, manager}) => {
+                        console.log(firstName, lastName, role, manager)
+                        Prompt();
+                    })
+            }
+            else if (selection === options[6]) {
+                console.log('Update An Employee Role was selected')
+                return inquirer
+                .prompt()
+            }
+
+        })
+        
+}
+
+Prompt();
